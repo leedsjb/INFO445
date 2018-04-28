8ff480e2a52e59368d8941ffd87eaf5af317c720
J. Benjamin Leeds <leeds@uw.edu>
2018-04-27 18:29:47 -0700

    update SQL_reference

--- SQL_Reference.sql
+++ SQL_Reference.sql
@@ -3,24 +3,33 @@ SQL Reference
 INFO 445
 J. Benjamin Leeds
 Created: April 13, 2018
-Updated:
+Updated: Friday April 27, 2018
 
 Note: Unless otherwise specified these SQL queries are for T-SQL
 
 */
 
+-- select database to use BEFORE running queries below
+USE Lab3_445_leeds
 
 -- View all tables
 SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'
+-- OR w/ all details
+SELECT * FROM sys.tables
 
 -- List all stored procedures
-USE leeds_Lab2
 SELECT * FROM sys.procedures
 
 -- List column names and types of all table starting with 'tbl'
-SELECT COLUMN_NAME, TABLE_NAME, DATA_TYPE
+SELECT COLUMN_NAME, TABLE_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME LIKE 'tbl%'
 
 -- Display TOP N results from query
 SELECT TOP([N]) * FROM [dbo.tblName]
+
+-- Display all constraints in database:
+SELECT * FROM sys.objects WHERE type_desc LIKE '%CONSTRAINT';
+
+-- Display active connections to databases
+EXECUTE sp_who;
